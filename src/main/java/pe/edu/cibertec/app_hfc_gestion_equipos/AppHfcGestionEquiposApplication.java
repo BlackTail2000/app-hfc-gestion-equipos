682af6d3cfbb4a37825140411b84c2121d0dc4ac
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-12 00:32:02 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_hfc_gestion_equipos/AppHfcGestionEquiposApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_hfc_gestion_equipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHfcGestionEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppHfcGestionEquiposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_hfc_gestion_equipos/AppHfcGestionEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_hfc_gestion_equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHfcGestionEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
